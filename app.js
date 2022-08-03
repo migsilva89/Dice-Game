5a5c43b781c586258aced583a5a8f96c816b70dc
migsilva89 <migdrum@gmail.com>
2022-08-03 20:15:29 +0200

    sumResultArr() + saveFInalResult() + fix rollDice()

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
-let dice = document.getElementById('dice');
-var outputDiv = document.getElementById('diceResult');
-var finalResult = []
+let dice = document.getElementById('dice')
+let holdResult = document.getElementById('holdResult')
+let actualResultPlayerOne = document.getElementById('actualResultPlayerOne')
+let actualResultPlayerTwo = document.getElementById('actualResultPlayerTwo')
+let waitingResult = []
+let finalPlayerOneResult = []
+let finalPlayerTwoResult = []
+let currentPlayer = "0"
 
 function rollDice() {
-   
     let result = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
     dice.dataset.side = result;
     dice.classList.toggle("reRoll");
+    
+    if (result != 1) {
+        waitingResult.push(result) 
+    } else {
+        waitingResult = [0]
+        if (currentPlayer == "0") {
+            currentPlayer = "1"
+        } else {
+            currentPlayer = "0"
+        }
+    }  
+    saveFinalResult(waitingResult, currentPlayer, result)
+}
 
-    finalResult.push(result)
-    outputDiv.classList.remove("reveal");
-    outputDiv.classList.add("hide");
-    outputDiv.innerHTML = "You've got " + result;
-
+function sumResultsArr(result) {
     let sum = 0;
-
-    for (let i = 0; i < finalResult.length; i++) {
-        sum += finalResult[i];
+    for (let i = 0; i < result.length; i++) {
+        sum += result[i];
     }
-    console.log(sum, finalResult);
+    return sum
+}
 
-    setTimeout(function(){ outputDiv.classList.add("reveal"); }, 1500);
+function saveFinalResult(waitingResult, activePlayer, result ) {
+    if (activePlayer == "0") {
+        
+        if (result != 1) {
+            console.log(`player 1 and the number is ${result}`)
+            actualResultPlayerOne.innerHTML = sumResultsArr(waitingResult)
+        } else {
+            console.log("YOU LOSE PLAYER 2")
+            actualResultPlayerTwo.innerHTML = 0
+        }
+
+    } else {
+        if (result != 1) {
+            console.log(`player 2 and the number is ${result}`)   
+            actualResultPlayerTwo.innerHTML = sumResultsArr(waitingResult)
+        } else {
+            console.log("YOU LOSE PLAYER 1")
+            actualResultPlayerOne.innerHTML = 0
+        }
+    }
 }
 
 dice.addEventListener("click", rollDice);
-
